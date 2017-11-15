1c49e18d7bbf6e98b1b1d92ecf7d6cd4aeab34e4
Matthias Jakob <schotte93@googlemail.com>
2017-11-15 14:08:39 +0100

    ram_monitor is sending on a shared topic, redefinde msg

--- src/rammonitor.cpp
+++ src/rammonitor.cpp
@@ -8,6 +8,11 @@
 #include <proc/sysinfo.h>
 #include "rammonitor.h"
 #include "std_msgs/Float32.h"
+#include "ros_monitoring/MonitoringInfo.h"
+#include "string.h"
+
+
+#include "help.cpp"
 
 RAMMonitor::RAMMonitor() {
 	// TODO Auto-generated constructor stub
@@ -22,6 +27,7 @@ int main(int argc, char **argv) {
 
 	ros::init(argc, argv, "ram_monitor");
 	ros::NodeHandle n("~");
+	ros::Publisher monitor_pub = n.advertise<ros_monitoring::MonitoringInfo>("/monitoring/all", 1);
 	ros::Publisher used_pub;
 	ros::Publisher percentage_pub;
 	std_msgs::Float32 percentage;
@@ -52,6 +58,10 @@ int main(int argc, char **argv) {
 
 	while (ros::ok()) {
 		meminfo();
+		ros_monitoring::MonitoringInfo mi;
+		mi.name = ros::this_node::getName();
+		mi.description = "A RAM-Monitor";
+		fillMachineInfo(mi);
 
 		/*	kb_main_total, kb_main_used, kb_main_free,
 		 kb_main_shared, kb_main_buffers, kb_main_cached*/
@@ -65,6 +75,26 @@ int main(int argc, char **argv) {
 					* 100.0;
 			percentage_pub.publish(percentage);
 		}
+		ros_monitoring::KeyValue used;
+		used.key = "used";
+		char value[200];
+		sprintf(value, "%d", (long) kb_main_used);
+
+		used.value = value;
+		mi.values.push_back(used);
+
+		ros_monitoring::KeyValue percent;
+		percent.key = "percent";
+		sprintf(value, "%f", percentage.data);
+		percent.value = value;
+		if(percentage.data > 70) {
+			percent.error.level = 0.7;
+		} else {
+			percent.error.level = 0;
+		}
+		mi.values.push_back(percent);
+
+		monitor_pub.publish(mi);
 
 		loop_rate.sleep();
 
