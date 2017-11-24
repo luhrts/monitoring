e898562721f98ed4bdfde19a3303f0ad25e164ea
Matthias Jakob <schotte93@googlemail.com>
2017-11-24 10:43:37 +0100

    topicmonitor calculates the frequency over last 10 msgs , so that it is faster changing its values

--- src/topicmonitor.py
+++ src/topicmonitor.py
@@ -12,6 +12,8 @@ def topicmonitor():
     frequency = rospy.get_param(rospy.get_name() + '/frequency')
     rate = rospy.Rate(frequency)
     
+    lastXvaluesForCalc = 10
+    
     hzMonitors = {}
     for entry in topics:
         rthz = ROSTopicHz(-1, filter_expr=None)
@@ -31,12 +33,14 @@ def topicmonitor():
         
         for entry in topics:
             n = len(hzMonitors[entry['name']].times) 
+            if(n<lastXvaluesForCalc):
+                continue
             if(n==0):   #no msgs received, division by 0 catching
                 rospy.logwarn("no new MSGS for topic %s", entry['name'] )
                 continue
-            mean = sum(hzMonitors[entry['name']].times) / n #TODO this is mean overall time. Needs mean over since last seconds
+            mean = sum(hzMonitors[entry['name']].times[-lastXvaluesForCalc:]) / lastXvaluesForCalc #TODO this is mean overall time. Needs mean over since last seconds
             freq = 1./mean if mean > 0. else 0 
-            if(entry['frequency']-0.1 > freq):  #TODO send warning on topic
+            if(entry['frequency']-0.1 > freq):  
                 rospy.logwarn("Frequency of %s is to low: Expected: %f Actual: %f",entry['name'] ,entry['frequency'] ,freq)
                 kv = KeyValue()
                 kv.key = "slow Topic"
