8350af8e89d1959c567d309dc98506ee1cb31b86
Matthias Jakob <schotte93@googlemail.com>
2017-12-04 16:42:17 +0100

    first development of a user defined fdi

--- src/fdir/userfaultdetection.cpp
+++ src/fdir/userfaultdetection.cpp
@@ -7,14 +7,114 @@
 
 #include "userfaultdetection.h"
 
-UserFaultDetection::UserFaultDetection()
+UserFaultDetection::UserFaultDetection(ros::NodeHandle& n)
 {
-  // TODO Auto-generated constructor stub
+  sub = n.subscribe("/monitoring/all", 1000, &UserFaultDetection::monitorCallback, this);
+  pub = n.advertise<ros_monitoring::Error>("/monitoring/errors", 1000);
 
 }
 
 UserFaultDetection::~UserFaultDetection()
 {
   // TODO Auto-generated destructor stub
+}
+
+void UserFaultDetection::monitorCallback(ros_monitoring::MonitoringInfo mi)
+{
+  ROS_INFO("CALLBACK");
+  for (int i = 0; i < mi.values.size(); i++)
+  {
+    msgBuffer.push(mi.values[i]);
+  }
+}
+
+void UserFaultDetection::load_config(ros::NodeHandle& n)
+{
+  std::vector < std::string > faultdetection;
+  if (n.getParam("faultdetection", faultdetection))
+  {
+  }
+
+  for (int i = 0; i < faultdetection.size(); i++)
+  {
+    ROS_INFO("%s", faultdetection[i].c_str());
+    fdiconfig newConfig;
+    if (!n.getParam(faultdetection[i] + "/operator", newConfig.op))
+    {
+      ROS_ERROR("The supplied Type \"%s\" is not available!", faultdetection[i].c_str());
+      continue;
+    }
+
+    if (!n.getParam(faultdetection[i] + "/value", newConfig.value))
+    {
+      ROS_ERROR("value error");
+    }
+    if (!n.getParam(faultdetection[i] + "/error", newConfig.error))
+    {
+      ROS_ERROR("error error");
+    }
+    if (!n.getParam(faultdetection[i] + "/errorlevel", newConfig.errorlevel))
+    {
+      ROS_ERROR("errorlevel error");
+    }
+    std::string message;
+    if (!n.getParam(faultdetection[i] + "/message", message))
+    {
+      ROS_ERROR("message error");
+    }
+
+    user_config[message] = newConfig;
+  }
+
+}
+
+void UserFaultDetection::fdi()
+{
+  ROS_INFO("FDI:");
+  while (!msgBuffer.empty())
+  {
+    ros_monitoring::KeyValue kv = msgBuffer.front();
+//    ROS_INFO("Key: %s", kv.key.c_str());
+    if (!(user_config.find(kv.key) == user_config.end()))
+    {
+      //TODO check the config and publish msg if error
+      fdiconfig config = user_config[kv.key];
+      //ROS_INFO("For message: \'%s\' following config", kv.key.c_str());
+      if(kv.value>config.value) {
+
+      }
+
+    }
+    else {
+//      ROS_INFO("No Key found");
+    }
+
+    msgBuffer.pop();
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "user_fdi");
+  ros::NodeHandle n("~");
+
+  float freq = 1;
+  if (!n.getParam("frequency", freq))
+  {
+    ROS_WARN("No frequency supplied. Working with %d Hz.", freq);
+  }
+
+  UserFaultDetection userFDI(n);
+
+  ros::Rate loop_rate(freq);
+
+  userFDI.load_config(n);
+  while (ros::ok())
+  {
+    userFDI.fdi();
+    loop_rate.sleep();
+    ros::spinOnce();
+  }
+
 }
 
--- src/fdir/userfaultdetection.h
+++ src/fdir/userfaultdetection.h
@@ -8,11 +8,35 @@
 #ifndef SRC_FDIR_USERFAULTDETECTION_H_
 #define SRC_FDIR_USERFAULTDETECTION_H_
 
+#include "ros/ros.h"
+#include <queue>
+#include "ros_monitoring/MonitoringInfo.h"
+#include "ros_monitoring/Error.h"
+#include "string"
+
+struct fdiconfig{
+  std::string op;
+  float value;
+  std::string error;
+  float errorlevel;
+};
+
 class UserFaultDetection
 {
 public:
-  UserFaultDetection();
+  UserFaultDetection(ros::NodeHandle& n);
   virtual ~UserFaultDetection();
+
+  void load_config(ros::NodeHandle& n);
+  void fdi();
+
+private:
+  void monitorCallback(ros_monitoring::MonitoringInfo mi);
+  std::queue<ros_monitoring::KeyValue> msgBuffer;
+  ros::Subscriber sub;
+  ros::Publisher pub;
+
+  std::map<std::string, fdiconfig> user_config;
 };
 
 #endif /* SRC_FDIR_USERFAULTDETECTION_H_ */
