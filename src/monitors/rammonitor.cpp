ad028d78aa00cf0be5e1c688fbbfcd23f411b1ad
Matthias Jakob <schotte93@gmail.com>
2018-04-20 11:46:04 +0200

    cleaned up ram monitor + refeactored to monitormsg

--- src/monitors/rammonitor.cpp
+++ src/monitors/rammonitor.cpp
@@ -13,14 +13,14 @@
 #include "ros_rt_benchmark_lib/benchmark.h"
 
 #include "ros_monitoring/help.h"
+#include "ros_monitoring/monitors/monitormsg.h"
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "ram_monitor");
   ros::NodeHandle n("~");
   ros::Publisher monitor_pub = n.advertise<ros_monitoring::MonitoringArray>("/monitoring", 1);
-  ros::Publisher used_pub, percentage_pub;
-  std_msgs::Float32 percentage, used;
+
 
   float freq = 1;
   if (!n.getParam("frequency", freq))
@@ -31,81 +31,39 @@ int main(int argc, char **argv)
   bool bUsed = false;
   if (n.getParam("used", bUsed))
   {
-//		if (bUsed) {
-//			used_pub = n.advertise<std_msgs::Float32>("/monitoring/ram/used",
-//					1);
-//		}
   }
 
   bool bPercent = false;
   if (n.getParam("percent", bPercent))
   {
-//		if (bPercent) {
-//			percentage_pub = n.advertise<std_msgs::Float32>(
-//					"/monitoring/ram/percentage", 1);
-//		}
   }
 
   ros::Rate loop_rate(freq);
 
-  //benchmark init dings
-  ROS_RT_Benchmark benchmark;
-  benchmark.init();
-  ROS_RT_MeasurementDuration* measurement_meminfo = benchmark.createDurationMeasurement("meminfo");
-
+  MonitorMsg msg(n, ros::this_node::getName(), "RAM-Monitor" );
   while (ros::ok())
   {
-    measurement_meminfo->start();
+    msg.resetMsg();
+
     meminfo();		//geting ram info via sysinfo lib
-    measurement_meminfo->stop();
-
-    ros_monitoring::MonitoringArray ma;
-	ros_monitoring::MonitoringInfo mi;
-
-    mi.name = ros::this_node::getName();
-    mi.description = "A RAM-Monitor";
-    fillMachineInfo(mi);
+    msg.addNewInfoTree(ros::this_node::getName(), "RAM-Monitor");
 
     /*	kb_main_total, kb_main_used, kb_main_free,
      kb_main_shared, kb_main_buffers, kb_main_cached*/
     char value[200];
     if (bUsed)
     {
-
-//			used.data = (float) kb_main_used;
-//			used_pub.publish(used);
-
-      ros_monitoring::KeyValue used;
-      used.key = "ram used";
-
-      sprintf(value, "%ld", (long)kb_main_used);
-
-      used.value = value;
-      used.unit = "kb";
-      mi.values.push_back(used);
-
+      msg.addValue("RAM used", kb_main_used, "kb", 0);
     }
     if (bPercent)
     {
-
       float perc = ((float)kb_main_used / (float)kb_main_total) * 100.0;
-      /*percentage.data = perc
-       percentage_pub.publish(percentage);*/
-
-      ros_monitoring::KeyValue percent;
-      percent.key = "ram percentage used";
-      sprintf(value, "%f", perc);
-      percent.value = value;
-      percent.unit = "%";
-      mi.values.push_back(percent);
-
+      msg.addValue("RAM % used", perc, "%", 0);
     }
-    ma.info.push_back(mi);
-    monitor_pub.publish(ma);
+    msg.publish();
 
     loop_rate.sleep();
 
   }
 
-  benchmark.logData();
 }
