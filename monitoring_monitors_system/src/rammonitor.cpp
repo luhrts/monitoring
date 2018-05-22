08358db5c6ec2c20ba1068a3358acfbfe236f072
Matthias Jakob <schotte93@googlemail.com>
2018-05-22 12:13:34 +0200

    started refeactoring of system monitors

--- monitoring_monitors_system/src/rammonitor.cpp
+++ monitoring_monitors_system/src/rammonitor.cpp
@@ -1,13 +1,41 @@
-/*
- * rammonitor.cpp
+/*********************************************************************
+ * Software License Agreement (BSD License)
  *
- *  Created on: Nov 3, 2017
- *      Author: matthias
- */
+ * Copyright (c) 2018  University of Hannover
+ *                     Institute for Systems Engineering - RTS
+ *                     Prof. Dr.-Ing. Bernardo Wagner
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name University of Hannover nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
 #include "ros/ros.h"
 #include <proc/sysinfo.h>
-#include "std_msgs/Float32.h"
-#include "string.h"
 
 #include "monitoring_core/monitor.h"
 
@@ -15,8 +43,6 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "ram_monitor");
   ros::NodeHandle n("~");
-  ros::Publisher monitor_pub = n.advertise<monitoring_msgs::MonitoringArray>("/monitoring", 1);
-
 
   float freq = 1;
   if (!n.getParam("frequency", freq))
@@ -42,10 +68,9 @@ int main(int argc, char **argv)
     msg.resetMsg();
 
     meminfo();		//geting ram info via sysinfo lib
-//    msg.addNewInfoTree(ros::this_node::getName(), "RAM-Monitor");
-
-    /*	kb_main_total, kb_main_used, kb_main_free,
+    /* OUTPUT: kb_main_total, kb_main_used, kb_main_free,
      kb_main_shared, kb_main_buffers, kb_main_cached*/
+
     char value[200];
     if (bUsed)
     {
