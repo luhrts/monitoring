1f737b83a85ed68e813b05951947be5bb5c5c09e
Jan Carstensen <git@jancarstensen.com>
2018-05-22 07:26:26 +0200

    Initial commit

---
+++ monitoring_core/src/monitor.cpp
@@ -0,0 +1,54 @@
+/*
+ * Monitor.cpp
+ *
+ *  Created on: Feb 5, 2018
+ *      Author: matthias
+ */
+
+#include "monitoring_core/monitor.h"
+
+Monitor::Monitor(ros::NodeHandle &n, std::string monitorDescription) {
+  pub = n.advertise<monitoring_msgs::MonitoringArray> ("/monitoring", 1);
+  monitoring_msgs::MonitoringInfo mi;
+  mi.name = n.getNamespace();
+  mi.description = monitorDescription;
+  ma.info[miIndex] = mi;
+}
+
+Monitor::~Monitor() {
+	// TODO Auto-generated destructor stub
+
+}
+
+void Monitor::addValue(std::string key, std::string value, std::string unit, float errorlevel) {
+	if(miIndex<0){
+		ROS_ERROR("No Info Tree Node Created!!! Use addNewInfoTree before adding Values");
+	} else {
+    monitoring_msgs::KeyValue kv;
+		kv.key = key;
+		kv.value = value;
+		kv.unit = unit;
+		kv.errorlevel = errorlevel;
+		ma.info[miIndex].values.push_back(kv);
+	}
+}
+
+void Monitor::addValue(std::string key, float value, std::string unit, float errorlevel){
+  char stringvalue[100];    //TODO check if 100 is a good number!
+	sprintf(stringvalue, "%f", value);
+	addValue(key, stringvalue, unit, errorlevel);
+}
+
+void Monitor::publish() {
+	ma.header.stamp = ros::Time::now();
+	pub.publish(ma);
+
+	resetMsg();
+}
+
+void Monitor::resetMsg(){
+  monitoring_msgs::MonitoringArray newMA;
+	ma = newMA;
+	miIndex = -1;
+
+}
