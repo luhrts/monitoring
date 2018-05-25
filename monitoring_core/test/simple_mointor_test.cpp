79bc7abdca8c3f72ec0653c14da8dd96224a9a69
Gao <ft1148137@gmail.com>
2018-05-25 15:01:02 +0200

    Test_Gao

--- monitoring_core/test/simple_mointor_test.cpp
+++ monitoring_core/test/simple_mointor_test.cpp
@@ -1,28 +1,55 @@
 #include <gtest/gtest.h>
-#include <cmath>
+#include <ros/ros.h>
+#include <monitoring_core/monitor.h>
 
-double squareRoot(const double a) {
-  double b = sqrt(a);
-  if(b != b) { // nan check
-      return -1.0;
-  }else{
-      return sqrt(a);
+
+TEST(MonitoringCore, addValueString) {
+    ros::NodeHandle nh;
+    Monitor monitor_1(nh, "String_Test");
+
+  for(int i=0;i<500;i++)
+  {
+      string Test_key(i, 'a');
+      string Test_unit(i, 'b');
+      string Test_value(i, 'b');
+
+
+
+
+    monitor_1.addValue(Test_key, Test_value, Test_unit,0.0);
+
+    ASSERT_TRUE(true);
   }
 }
 
-TEST(SquareRootTest, PositiveNos) {
-    ASSERT_EQ(6, squareRoot(36.0));
-    ASSERT_EQ(18.0, squareRoot(324.0));
-    ASSERT_EQ(25.4, squareRoot(645.16));
-    ASSERT_EQ(0, squareRoot(0.0));
+TEST(MonitoringCore, addValueFloat) {
+    ros::NodeHandle nh;
+
+    Monitor monitor_2(nh, "Float_Test");
+
+    monitor_1.addValue("char", y, "Char unit",0.0);
+    monitor_1.publish();
+
+    ASSERT_TRUE(true);
 }
 
-TEST(SquareRootTest, NegativeNos) {
-    ASSERT_EQ(-1.0, squareRoot(-15.0));
-    ASSERT_EQ(-1.0, squareRoot(-0.2));
+TEST(MonitoringCore, addValueString_empty) {
+    ros::NodeHandle nh;
+
+    Monitor monitor_1(nh, "Gao_monitor_int_example");
+
+    std::string y="";
+
+    monitor_1.addValue("char", y, "Char unit",0.0);
+    monitor_1.publish();
+
+    ASSERT_TRUE(true);
 }
 
 int main(int argc, char **argv) {
+    ros::init(argc, argv, "Gao_monitor");
+
+
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
--- monitoring_examples/src/simple_monitor.cpp
+++ monitoring_examples/src/simple_monitor.cpp
@@ -3,17 +3,35 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "simple_monitor");
+  ros::init(argc, argv, "Gao_monitor");
   ros::NodeHandle nh;
 
-  Monitor monitor(nh, "simple_monitor_example");
+  Monitor monitor_1(nh, "Gao_monitor_int_example");
+  Monitor monitor_2(nh, "Gao_monitor_string_example");
+
 
   ros::Rate loop_rate(10);
-  int c = 0;
+  double E_level;
+  int x = 0;
+  std::string y="Hello monitor";
   while (ros::ok()){
-    monitor.addValue("count", c, "SIUnits", 0.0);
-    c++;
-    monitor.publish();
+      if(x>=20)
+      {
+          E_level=1.0;
+        if(x>=40)
+        {
+            E_level=2.0;
+        }
+      }
+      else
+           E_level=0.0;
+
+    monitor_2.addValue("count_level", x, "Number unit",E_level);
+    monitor_1.addValue("char", y, "Char unit",0.0);
+    x++;
+    monitor_1.publish();
+    monitor_2.publish();
+
     loop_rate.sleep();
   }
 
