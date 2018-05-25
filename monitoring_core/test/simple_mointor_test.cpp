77b176d70ffa7ac2ea8c934d4e8220b2736b9a71
Gao <ft1148137@gmail.com>
2018-05-25 20:27:13 +0200

    Test for Monitore_core is finish

--- monitoring_core/test/simple_mointor_test.cpp
+++ monitoring_core/test/simple_mointor_test.cpp
@@ -58,11 +58,12 @@ TEST(MonitoringCore, addValueFloat)
     ros::NodeHandle nh;
     Monitor monitor_5(nh, "Float_+_Test");
     Monitor monitor_6(nh, "Float_-_Test");
+    float RandemNumber1;
+    float RandemNumber2;
     for(int i=0;i<1000;i++)
     {
         float x=123.123*i;
-        float RandemNumber1;
-        float RandemNumber2;
+
 
         monitor_5.addValue("Float_Test",x, "Float_Test",0.0);
         monitor_6.addValue("Float_Test",-x, "Float_Test",0.0);
@@ -85,9 +86,10 @@ TEST(MonitoringCore, addValueErrorLevel)
 
     for(int i=0;i<=4;i++)
     {
-       float y=0;
+       float y=0.0;
        ++y;
        monitor_7.addValue("ErrorLevel_Test",0.0, "ErrorLevel_Test",y);
+       ASSERT_EQ(monitor_7.ma.info[0].values[i].errorlevel,y);
 
       ASSERT_TRUE(true);
     }
