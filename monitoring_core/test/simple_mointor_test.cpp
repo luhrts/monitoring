41c69e8011595010ab3042c963d9091cb6b35194
Gao <ft1148137@gmail.com>
2018-05-25 15:35:54 +0200

    Gao_test

--- monitoring_core/test/simple_mointor_test.cpp
+++ monitoring_core/test/simple_mointor_test.cpp
@@ -3,15 +3,16 @@
 #include <monitoring_core/monitor.h>
 
 
-TEST(MonitoringCore, addValueString) {
+TEST(MonitoringCore, addValueString)
+{
     ros::NodeHandle nh;
     Monitor monitor_1(nh, "String_Test");
 
   for(int i=0;i<500;i++)
   {
-      string Test_key(i, 'a');
-      string Test_unit(i, 'b');
-      string Test_value(i, 'b');
+      std::string Test_key(i, 'a');
+      std::string Test_unit(i, 'b');
+      std::string Test_value(i, 'b');
 
 
 
@@ -22,28 +23,57 @@ TEST(MonitoringCore, addValueString) {
   }
 }
 
-TEST(MonitoringCore, addValueFloat) {
+TEST(MonitoringCore, addValueFloatRand)
+{
     ros::NodeHandle nh;
 
-    Monitor monitor_2(nh, "Float_Test");
+    Monitor monitor_2(nh, "Float_Max_Rand_Test");
+    Monitor monitor_3(nh, "Float_Min_Rand_Test");
+    Monitor monitor_4(nh, "Float_null_Rand_Test");
 
-    monitor_1.addValue("char", y, "Char unit",0.0);
-    monitor_1.publish();
+
+    monitor_2.addValue("Float_Test",3.402823466e+38f, "Float_Test",0.0);
+    monitor_3.addValue("Float_Test",1.175494351e-38f, "Float_Test",0.0);
+    monitor_4.addValue("Float_Test",0, "Float_Test",0.0);
+
 
     ASSERT_TRUE(true);
 }
 
-TEST(MonitoringCore, addValueString_empty) {
+TEST(MonitoringCore, addValueFloat)
+{
     ros::NodeHandle nh;
 
-    Monitor monitor_1(nh, "Gao_monitor_int_example");
+    for(int i=0;i<20;i++)
+    {
+        float x=1234.1234*i;
+        Monitor monitor_5(nh, "Float_+_Test");
+        Monitor monitor_6(nh, "Float_-_Test");
 
-    std::string y="";
 
-    monitor_1.addValue("char", y, "Char unit",0.0);
-    monitor_1.publish();
+        monitor_5.addValue("Float_Test",x, "Float_Test",0.0);
+        monitor_6.addValue("Float_Test",-x, "Float_Test",0.0);
 
-    ASSERT_TRUE(true);
+
+        ASSERT_TRUE(true);
+    }
+
+}
+TEST(MonitoringCore, addValueErrorLevel) {
+    ros::NodeHandle nh;
+
+    Monitor monitor_7(nh, "ErrorLevel_Test");
+
+    for(int i=0;i<=4;i++)
+    {
+       float y=0;
+       ++y;
+       monitor_7.addValue("ErrorLevel_Test",0.0, "ErrorLevel_Test",y);
+
+      ASSERT_TRUE(true);
+    }
+
+
 }
 
 int main(int argc, char **argv) {
