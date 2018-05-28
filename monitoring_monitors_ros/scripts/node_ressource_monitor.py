7642e45dc173a799696e5745b97e778b3c293a46
Michael Lange <michael@ubuntu.ubuntu-domain>
2018-05-28 16:21:21 +0200

    changed function to not only print to console but also send values to monitoring system

--- monitoring_monitors_ros/scripts/node_ressource_monitor.py
+++ monitoring_monitors_ros/scripts/node_ressource_monitor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """
  Author: Michael Lange
+ pass the data to the ros monitoring system
  This scripts lists all running rosnodes and displays all values the
  psutil package is providing for each node.
  Supports a filter function in order to only retrieve information of specific
@@ -25,7 +26,7 @@ import rosnode
 import xmlrpclib
 
 from std_msgs.msg import String
-
+from monitoring_core.monitor import Monitor
 from collections import namedtuple
 
 ID = "NODEINFO"
@@ -117,9 +118,10 @@ def get_process_info(pid):
     #return namespace
     return ret_val
 
-def print_info_to_console(name, pid):
+def print_to_console_and_monitor(name, pid):
     """
     print information to console
+    pass the data to the ros monitoring system
     """
     #get all values belonging to pid
     try:
@@ -127,6 +129,7 @@ def print_info_to_console(name, pid):
     except psutil._exceptions.NoSuchProcess:
         pass
 
+    monitor = Monitor("node_ressource_monitor")
     no_param_available = False
     #get list of whitelisted nodes
     try:
@@ -159,71 +162,106 @@ def print_info_to_console(name, pid):
             for element in whitelisted_values.get("values"):
                 if element == 'children':
                     rospy.loginfo("children: " + str(node_info_tuple.children))
+                    monitor.addValue("children", str(node_info_tuple.children), "", 0.0)
                 if element == 'cmdline':
                     rospy.loginfo("cmdline: " + str(node_info_tuple.cmdline))
+                    monitor.addValue("cmdline", str(node_info_tuple.cmdline), "", 0.0)
                 if element == 'connections':
                     rospy.loginfo("connections: " + str(node_info_tuple.connections))
+                    monitor.addValue("connections", str(node_info_tuple.connections), "", 0.0)
                 if element == 'cpu_affinity':
                     rospy.loginfo("cpu_affinity: " + str(node_info_tuple.cpu_affinity))
+                    monitor.addValue("cpu_affinity", str(node_info_tuple.cpu_affinity), "", 0.0)
                 if element == 'cpu_percent':
                     rospy.loginfo("cpu_percent: " + str(node_info_tuple.cpu_percent))
+                    monitor.addValue("cpu_percent", str(node_info_tuple.cpu_percent), "", 0.0)
                 if element == 'cpu_times':
                     rospy.loginfo("cpu_times: " + str(node_info_tuple.cpu_times))
+                    monitor.addValue("cpu_times", str(node_info_tuple.cpu_times), "", 0.0)
                 if element == 'create_time':
                     rospy.loginfo("create_time: " + str(node_info_tuple.create_time))
+                    monitor.addValue("create_time", str(node_info_tuple.create_time), "", 0.0)
                 if element == 'cwd':
                     rospy.loginfo("cwd: " + str(node_info_tuple.cwd))
+                    monitor.addValue("cwd", str(node_info_tuple.cwd), "", 0.0)
                 if element == 'exe':
                     rospy.loginfo("exe-path: " + str(node_info_tuple.exe))
+                    monitor.addValue("exe-path", str(node_info_tuple.exe), "", 0.0)
                 if element == 'gids':
                     rospy.loginfo("gids: " + str(node_info_tuple.gids))
+                    monitor.addValue("gids", str(node_info_tuple.gids), "", 0.0)
                 if element == 'io_counters':
                     rospy.loginfo("io_counters: " + str(node_info_tuple.io_counters))
+                    monitor.addValue("io_counters", str(node_info_tuple.io_counters), "", 0.0)
                 if element == 'ionice':
                     rospy.loginfo("ionice: " + str(node_info_tuple.ionice))
+                    monitor.addValue("ionice", str(node_info_tuple.ionice), "", 0.0)
                 if element == 'is_running':
                     rospy.loginfo("is_running: " + str(node_info_tuple.is_running))
+                    monitor.addValue("is_running", str(node_info_tuple.is_running), "", 0.0)
                 if element == 'memory_info':
                     rospy.loginfo("memory_info: " + str(node_info_tuple.memory_info))
+                    monitor.addValue("memory_info", str(node_info_tuple.memory_info), "", 0.0)
                 if element == 'memory_info_ex':
                     rospy.loginfo("memory_info_ex: " + str(node_info_tuple.memory_info_ex))
+                    monitor.addValue("memory_info_ex", str(node_info_tuple.memory_info_ex), "", 0.0)
                 if element == 'memory_maps':
                     rospy.loginfo("memory_maps: " + str(node_info_tuple.memory_maps))
+                    monitor.addValue("memory_maps", str(node_info_tuple.memory_maps), "", 0.0)
                 if element == 'memory_percent':
                     rospy.loginfo("memory_percent: " + str(node_info_tuple.memory_percent))
+                    monitor.addValue("memory_percent", str(node_info_tuple.memory_percent), "", 0.0)
                 if element == 'name':
                     rospy.loginfo("name: " + str(node_info_tuple.name))
+                    monitor.addValue("name", str(node_info_tuple.name), "", 0.0)
                 if element == 'nice':
                     rospy.loginfo("nice: " + str(node_info_tuple.nice))
+                    monitor.addValue("nice", str(node_info_tuple.nice), "", 0.0)
                 if element == 'num_ctx_switches':
                     rospy.loginfo("num_ctx_switches: " + str(node_info_tuple.num_ctx_switches))
+                    monitor.addValue("num_ctx_switches", str(node_info_tuple.num_ctx_switches), "", 0.0)
                 if element == 'num_fds':
                     rospy.loginfo("num_fds: " + str(node_info_tuple.num_fds))
+                    monitor.addValue("num_fds", str(node_info_tuple.num_fds), "", 0.0)
                 if element == 'num_handles':
                     rospy.loginfo("num_handles: " + str(node_info_tuple.num_handles))
+                    monitor.addValue("num_handles", str(node_info_tuple.num_handles), "", 0.0)
                 if element == 'num_threads':
                     rospy.loginfo("num_threads: " + str(node_info_tuple.num_threads))
+                    monitor.addValue("num_threads", str(node_info_tuple.num_threads), "", 0.0)
                 if element == 'open_files':
                     rospy.loginfo("open_files: " + str(node_info_tuple.open_files))
+                    monitor.addValue("open_files", str(node_info_tuple.open_files), "", 0.0)
                 if element == 'parent':
                     rospy.loginfo("parent: " + str(node_info_tuple.parent))
+                    monitor.addValue("parent", str(node_info_tuple.parent), "", 0.0)
                 if element == 'pid':
                     rospy.loginfo("pid: " + str(node_info_tuple.pid))
+                    monitor.addValue("pid", str(node_info_tuple.pid), "", 0.0)
                 if element == 'ppid':
                     rospy.loginfo("ppid: " + str(node_info_tuple.ppid))
+                    monitor.addValue("ppid", str(node_info_tuple.ppid), "", 0.0)
                 if element == 'rlimit':
                     rospy.loginfo("rlimit: " + str(node_info_tuple.rlimit))
+                    monitor.addValue("rlimit", str(node_info_tuple.rlimit), "", 0.0)
                 if element == 'status':
                     rospy.loginfo("status: " + str(node_info_tuple.status))
+                    monitor.addValue("status", str(node_info_tuple.status), "", 0.0)
                 if element == 'terminal':
                     rospy.loginfo("terminal: " + str(node_info_tuple.terminal))
+                    monitor.addValue("terminal", str(node_info_tuple.terminal), "", 0.0)
                 if element == 'threads':
                     rospy.loginfo("threads: " + str(node_info_tuple.threads))
+                    monitor.addValue("threads", str(node_info_tuple.threads), "", 0.0)
                 if element == 'uids':
                     rospy.loginfo("uids: " + str(node_info_tuple.uids))
+                    monitor.addValue("uids", str(node_info_tuple.uids), "", 0.0)
                 if element == 'username':
                     rospy.loginfo("username: " + str(node_info_tuple.username))
+                    monitor.addValue("username", str(node_info_tuple.username), "", 0.0)
 
+            monitor.publish()
+            rospy.logout(monitor)
         else:
             rospy.logerr(name + " not in whitelist")
 
@@ -237,7 +275,7 @@ def gather_info():
     for i in node_list:
         try:
             get_process_info(i.pid)
-            print_info_to_console(i.name, i.pid)
+            print_to_console_and_monitor(i.name, i.pid)
             rospy.loginfo("------------------------------")
         except psutil._exceptions.NoSuchProcess:
             rospy.logerr("----------NO_SUCH_PROCESS_ERROR---------------")
