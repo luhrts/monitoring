5f32e50f5dea2d28f2415d9a894990dce46192ec
Gao <ft1148137@gmail.com>
2018-06-05 20:15:44 +0200

    test_for_ros_monitore

--- monitoring_monitors_ros/test/src/ros_statistics_monitor_test.cpp
+++ monitoring_monitors_ros/test/src/ros_statistics_monitor_test.cpp
@@ -48,14 +48,14 @@ public:
              if (keyValue.key == "Sub: "){
                    EXPECT_EQ(keyValue.value,"sub_for_test");
              }
-             if (keyValue.key == "Test_topic_string/TopicMissing"){
+             if (keyValue.key == "Test_topic_int16/TopicMissing"){
                   EXPECT_EQ(keyValue.errorlevel,1.0);
              }
-             if (keyValue.key == "Test_topic_string/frequency" && !keyValue.errorlevel == 0.5){
+             if (keyValue.key == "Test_topic_int16/frequency" && !keyValue.errorlevel == 0.5){
                     EXPECT_LE("10.2",keyValue.value);
                     EXPECT_GE("9.8",keyValue.value);
              }
-             if (keyValue.key == "Test_topic_string/frequency" && keyValue.errorlevel == 0.5){
+             if (keyValue.key == "Test_topic_int16/frequency" && keyValue.errorlevel == 0.5){
                     EXPECT_LE("9.8",keyValue.value);
                     EXPECT_GE("10.2",keyValue.value);
              }
