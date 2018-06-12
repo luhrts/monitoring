eba93009a61ac79c769808f934db05335c464b66
Matthias Jakob <schotte93@googlemail.com>
2018-06-12 16:08:54 +0200

    changed nodehandler interface

--- monitoring_fdir/src/recovery/recoveryExample.cpp
+++ monitoring_fdir/src/recovery/recoveryExample.cpp
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 
   RecoverySDK example(n);
 
-  RestartNodeHandler rnh;
+  RestartNodeHandler rnh("nodename");
   //StopLaunchFile slf;
   OutputErrorMessage oem;
   ErrorToSpeech ets(n);
