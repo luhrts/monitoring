d11dd4f3af26bfac3fc711c36b3bd1f8b8bdb23f
Jan Carstensen <carstensen@rts.uni-hannover.de>
2018-07-16 10:27:03 +0200

    fixed test merge

--- monitoring_monitors_ros/test/src/ros_statistics_monitor_test.cpp
+++ monitoring_monitors_ros/test/src/ros_statistics_monitor_test.cpp
@@ -1,82 +1,8 @@
-<<<<<<< HEAD
 #include <gtest/gtest.h>
-=======
-﻿#include <gtest/gtest.h>
->>>>>>> 7e980397b2465727873c6d2b1bf0f8bcda5b7b15
 #include <ros/ros.h>
 #include <monitoring_monitors_ros/statisticsmonitor.h>
 #include<iostream>
 
-<<<<<<< HEAD
-class Test_class
-{
-  ros::Subscriber Test_sub;
-  ros::Publisher Test_pub;
-
-
-public:
-  Test_class (ros::NodeHandle& n)
-  {
-    Test_pub = n.advertise<rosgraph_msgs::TopicStatistics> ("/statistics", 100);
-    Test_sub = n.subscribe("/monitoring", 100, &Test_class::TestCallback,this);
-
-  }
-
-  virtual ~Test_class(){}
-
-  void pub(rosgraph_msgs::TopicStatistics Test_ts)
-  {
-
-    ros::Duration(3).sleep();
-    ros::Rate loop_rate(10);
-
-    for(int i=0;i<=10;i++)
-    {
-      Test_pub.publish(Test_ts);
-      loop_rate.sleep();
-      ros::spinOnce();
-
-    }
-
-
-  }
-
-
-  void TestCallback(const monitoring_msgs::MonitoringArray& Callback_Test_msg)
-  {
-    // ASSERT_TRUE(false);
-
-    monitoring_msgs::MonitoringArray msg;
-    msg=Callback_Test_msg;
-
-    for(uint j=0;j<msg.info[0].values.size();j++)
-    {
-
-
-      if(msg.info[0].values[j].key == "Pub: ")
-      {
-
-        if(!(msg.info[0].values[j].value=="velodyne" || msg.info[0].values[j].value=="/teleop_turtle"))
-        {
-          ASSERT_TRUE(false);
-        }
-      }
-      if(msg.info[0].values[j].key == "Sub: ")
-      {
-        if(!(msg.info[0].values[j].value=="pc_to_pc2" || msg.info[0].values[j].value=="/turtlesim"))
-        {
-          ASSERT_TRUE(false);
-        }
-      }
-      if(msg.info[0].values[j].key == "Topic Missing")
-      {
-        ASSERT_EQ(msg.info[0].values[j].errorlevel,1.0);
-      }
-
-
-    }
-  }
-=======
 class Data_Check_class
 {
     ros::Subscriber Test_sub_for_ros_monitoring;
@@ -149,84 +75,18 @@ public:
       }
  }
 
->>>>>>> 7e980397b2465727873c6d2b1bf0f8bcda5b7b15
 };
 
 
 
 
-<<<<<<< HEAD
 
-TEST(Ros_Monitore, Monitore_Test_statistics_1)
-{
-  ros::NodeHandle nh;
-  Test_class Test_1(nh);
-  std::string Test_msgs;
-  std::string Test_Topic;
-
-
-
-  rosgraph_msgs::TopicStatistics Test_ts;
-  Test_Topic="/scan";
-  Test_ts.topic=Test_Topic;
-  Test_ts.node_pub="velodyne";
-  Test_ts.node_sub="pc_to_pc2";
-  // Test_ts.window_start=ros::Duration(0.5);
-  //Test_ts.window_stop=ros::Duration(0.0);
-  Test_ts.delivered_msgs=1;
-  Test_ts.dropped_msgs=1;
-  Test_ts.period_mean=ros::Duration(0.5);
-  Test_ts.traffic=1;
-  Test_ts.period_stddev=ros::Duration(0.1);
-  Test_ts.period_max=ros::Duration(1);
-  Test_ts.stamp_age_stddev=ros::Duration(0.1);
-  Test_ts.stamp_age_max=ros::Duration(1);
-
-  Test_1.pub(Test_ts);
-
-
-  ASSERT_TRUE(true);
-
-
-
-
-}
-
-TEST(Ros_Monitore, Monitore_Test_statistics_2)
-{
-  ros::NodeHandle nh;
-  Test_class Test_1(nh);
-  std::string Test_msgs;
-  std::string Test_Topic;
-  rosgraph_msgs::TopicStatistics Test_ts;
-  Test_Topic="/turtle1/cmd_vel";
-  Test_ts.topic=Test_Topic;
-  Test_ts.node_pub="/teleop_turtle";
-  Test_ts.node_sub="/turtlesim";
-  // Test_ts.window_start=ros::Duration(0.5);
-  //Test_ts.window_stop=ros::Duration(0.0);
-  Test_ts.delivered_msgs=1;
-  Test_ts.dropped_msgs=1;
-  Test_ts.period_mean=ros::Duration(0.5);
-  Test_ts.traffic=1;
-  Test_ts.period_stddev=ros::Duration(0.1);
-  Test_ts.period_max=ros::Duration(1);
-  Test_ts.stamp_age_stddev=ros::Duration(0.1);
-  Test_ts.stamp_age_max=ros::Duration(1);
-
-
-  Test_1.pub(Test_ts);
-
-
-  ASSERT_TRUE(true);
-=======
 TEST(Ros_Monitore, Monitore_Test_statistics_int16)
 {
     ros::NodeHandle nh;
     Data_Check_class Data_Check(nh);
     Data_Check.Data_check_topic_int16();
 
->>>>>>> 7e980397b2465727873c6d2b1bf0f8bcda5b7b15
 
 
 
@@ -234,24 +94,10 @@ TEST(Ros_Monitore, Monitore_Test_statistics_int16)
 
 
 
-
 int main(int argc, char **argv) {
-<<<<<<< HEAD
   ros::init(argc, argv, "ros_monitore_test");
   testing::InitGoogleTest(&argc, argv);
-=======
-    ros::init(argc, argv, "ros_monitore_test");
-    testing::InitGoogleTest(&argc, argv);
->>>>>>> 7e980397b2465727873c6d2b1bf0f8bcda5b7b15
 
-
-
-
-
-<<<<<<< HEAD
   return RUN_ALL_TESTS();
-=======
-    return RUN_ALL_TESTS();
->>>>>>> 7e980397b2465727873c6d2b1bf0f8bcda5b7b15
 }
 
