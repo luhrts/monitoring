6370d3fae66d61f14c47ff8f89bbe6b4cfb7f2d6
Gao <ft1148137@gmail.com>
2018-08-22 16:48:07 +0200

    opt topic monitor

--- monitoring_monitors_ros/scripts/topicmonitor.py
+++ monitoring_monitors_ros/scripts/topicmonitor.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
+import time
 import rospy
 from rostopic import *
 from time import sleep
 from monitoring_msgs.msg import *
 from monitoring_core.monitor import Monitor
-import time
+
 
 
 def topicmonitor():
@@ -18,39 +19,39 @@ def topicmonitor():
 
     rate = rospy.Rate(frequency)
     monitor = Monitor("simple_monitor_python")
-    
-    lastXvaluesForCalc = 10
-    
-    hzMonitors = {}
+    last_xvalues_for_calc = 10
+
+    hz_monitors = {}
     for entry in topics:
         rthz = ROSTopicHz(-1, filter_expr=None)
         print entry['name']
         sub = rospy.Subscriber(entry['name'], rospy.AnyMsg, rthz.callback_hz)
-        
-        hzMonitors[entry['name']] = rthz
-             
+        hz_monitors[entry['name']] = rthz
 
-    startTimestamp = time.time()
+    start_timestamp = time.time()
     while not rospy.is_shutdown():  # main loop
 
 
         for entry in topics:
-            n = len(hzMonitors[entry['name']].times) 
-            if(n < lastXvaluesForCalc):  # TODO was ist wenn keine miS gesendet werden. Dann wird nichts ueberprueft und somit keine Fehlermeldung rausgegeben!!! && Wenn bereits 10 narchichten da, aber keine neuen mehr ankommen?!?
-                if(time.time()-startTimestamp <=10): #nach 10 sekunden wird durchgeschaltet
+            n_ = len(hz_monitors[entry['name']].times) 
+            if n_ < last_xvalues_for_calc : 
+# TODO was ist wenn keine miS gesendet werden. Dann wird nichts ueberprueft und somit keine
+# Fehlermeldung rausgegeben!!! && Wenn bereits 10 narchichten da, aber keine neuen mehr
+# ankommen?!?
+                if time.time()-start_timestamp <=10 : #nach 10 sekunden wird durchgeschaltet
                     continue
-            if(n == 0):  # no mis received, division by 0 catching
+            if n_ == 0 :  # no mis received, division by 0 catching
                 rospy.logwarn("no new miS for topic %s", entry['name'])
                 value = "Topic " + entry['name'] + " sends no data"
                 monitor.addValue("no topic", value, "", 0.3)
                 continue
-            mean = sum(hzMonitors[entry['name']].times[-lastXvaluesForCalc:]) / lastXvaluesForCalc  # TODO this is mean overall time. Needs mean over since last seconds
+            mean = sum(hz_monitors[entry['name']].times[-last_xvalues_for_calc:]) / last_xvalues_for_calc  # TODO this is mean overall time. Needs mean over since last seconds
             freq = 1. / mean if mean > 0. else 0 
             if(entry['frequency'] - 0.1*freq > freq):  
                 rospy.logwarn("Frequency of %s is to low: Expected: %f Actual: %f", entry['name'] , entry['frequency'] , freq)
                 value = "Topic " + entry['name'] + " is on " + str(freq) + "Hz, Expected: " + str(entry['frequency'])
                 monitor.addValue("slow topic", value, "", 0.3)
-                
+
 
         rate.sleep()
 
--- monitoring_visualization/scripts/liveViewFloat.py
+++ monitoring_visualization/scripts/liveViewFloat.py
@@ -8,7 +8,7 @@ from matplotlib import style
 
 #TODO CLASSE!
 class LiveFloatGraph: 
- 
+
     def __init__(self, msg, ymin, warnlevel, cirticallevel, ymax):
         self.msgvalue = msg
         self.floatbuffer = []
@@ -35,7 +35,7 @@ class LiveFloatGraph:
         #self.axes.set_ylim([self.min,self.max])
         #plt.axhspan(self.warning, self.critical, color='yellow', alpha=0.3)
         #plt.axhspan(self.critical, self.max, color='red', alpha=0.3)
-   
+
     def callback(self, ma_):
 #         print self.floatbuffer
         for kv_ in ma_.info[0].values:
@@ -44,7 +44,7 @@ class LiveFloatGraph:
                 self.timestamp.append(float(ma_.info[0].header.stamp.secs 
 		+ (ma_.info[0].header.stamp.nsecs / 10**9)))
                 self.counterlist.append(self.counter)
-                
+
                 self.counter = self.counter+1
 
 if __name__ == '__main__':
@@ -54,12 +54,12 @@ if __name__ == '__main__':
         rospy.init_node("floatview")
         sub = rospy.Subscriber('/monitoring', MonitoringArray, fg.callback)
         rate = rospy.Rate(1)
-        
+
         fg.init_plot()
         while not rospy.is_shutdown():
             #print the value
             fg.update_plot()
             rate.sleep()
-        
+
     except rospy.ROSInterruptException:
         pass
