49b0d368a1eed88c851efc15ce3f8e75fd9f3231
Michael Lange <michael@ubuntu.ubuntu-domain>
2018-08-23 11:19:12 +0200

    added new bag file

--- monitoring_bag_plot/scripts/monitoring_bag_plot.py
+++ monitoring_bag_plot/scripts/monitoring_bag_plot.py
@@ -42,6 +42,7 @@ lined = {}
 #bag_dir = '/home/michael/youbot_local_dev/youbot_rosbag_20180816_secondbatterydrive/fahrt1.bag'
 bag_dir = '/home/michael/youbot_local_dev/youbot_rosbag_20180726_erstefahrt/fahrt3.bag'
 #bag_dir = '/home/michael/youbot_local_dev/youbot_rosbag_20180809_firstbatterydrive/fahrt1.bag'
+#bag_dir = '/home/michael/youbot_local_dev/youbot_rosbag_gui_cpu_test/test_mit_arm_alt_concat.bag'
 
 def init():
     rospy.init_node("monitoring_bag_plot")
