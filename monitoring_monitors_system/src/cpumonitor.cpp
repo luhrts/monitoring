70bf7b0254b2419a1ab654b091b255dff30b6e25
Gao <ft1148137@gmail.com>
2018-08-28 15:35:40 +0200

    fix error in cpp

--- monitoring_monitors_system/src/cpumonitor.cpp
+++ monitoring_monitors_system/src/cpumonitor.cpp
@@ -104,7 +104,7 @@ void CpuMonitor::init()
     sprintf(path, "/sys/class/hwmon/hwmon%d/name", i);
     fname = fopen(path, "r");
     char name[30];
-    fscanf(fname, "%30s", name);
+    fscanf(fname, "%29s", name);
     fclose(fname);
     if (strcmp(name, "coretemp") == 0)
     {	//check which is coretemp
