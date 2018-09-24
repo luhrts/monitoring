55a4aa20aee1f613f00b89053f59da52a0d88d03
Michael Lange <michael@ubuntu.ubuntu-domain>
2018-09-24 22:45:18 +0200

    updated map monitor

--- monitoring_monitors_ros/scripts/map_monitor.py
+++ monitoring_monitors_ros/scripts/map_monitor.py
@@ -11,10 +11,13 @@ from nav_msgs.msg import OccupancyGrid
 
 first_SLAM_map = True
 old_SLAM_map = None
+slam_total_changed_tiles = 0
 first_global_map = True
 old_global_map = None
+global_total_changed_tiles = 0
 first_explorationPlanner_map = True
 old_explorationPlanner_map = None
+exploration_total_changed_tiles = 0
 monitor = Monitor("map_monitor")
 
 def init():
@@ -28,24 +31,37 @@ def subscribe():
 def explorationPlanner_map_callback(msg):
     global first_explorationPlanner_map
     global old_explorationPlanner_map
+    global exploration_total_changed_tiles
     map_name = 'explorationPlanner_map'
-    old_explorationPlanner_map = map_monitor(msg, map_name, first_explorationPlanner_map, old_explorationPlanner_map)
+    old_explorationPlanner_map, changed_tiles = map_monitor(msg, map_name, first_explorationPlanner_map, old_explorationPlanner_map)
+    exploration_total_changed_tiles += changed_tiles
+    
+    monitor.addValue(map_name+"/total_changed_tiles",str(exploration_total_changed_tiles), "", 0)
+    monitor.publish()
     if first_explorationPlanner_map:
         first_explorationPlanner_map = False
 
 def global_map_callback(msg):
     global first_global_map
     global old_global_map
+    global global_total_changed_tiles
     map_name = 'global_map'
-    old_global_map = map_monitor(msg, map_name, first_global_map, old_global_map)
+    old_global_map, changed_tiles = map_monitor(msg, map_name, first_global_map, old_global_map)
+    global_total_changed_tiles += changed_tiles
+    monitor.addValue(map_name+"/total_changed_tiles",str(global_total_changed_tiles), "", 0)
+    monitor.publish()
     if first_global_map:
         first_global_map = False
 
 def SLAM_map_callback(msg):
     global first_SLAM_map
     global old_SLAM_map
+    global slam_total_changed_tiles
     map_name = 'SLAM_map'
-    old_SLAM_map = map_monitor(msg, map_name, first_SLAM_map, old_SLAM_map)
+    old_SLAM_map, changed_tiles = map_monitor(msg, map_name, first_SLAM_map, old_SLAM_map)
+    slam_total_changed_tiles += changed_tiles
+    monitor.addValue(map_name+"/total_changed_tiles",str(slam_total_changed_tiles), "", 0)
+    monitor.publish()
     if first_SLAM_map:
         first_SLAM_map = False
 
@@ -57,38 +73,38 @@ def map_monitor(msg, map_name, first_map, old_map):
     map_size = len(msg.data)
     #print map_size
     #print len(old_map) + ""+ map_name
+    changed_tiles = 0
     if first_map:
         first_map = False
         old_map = msg.data
-        return old_map
+        return old_map, changed_tiles
     l = len(old_map)
     if map_size != l:
         rospy.logerr("KACKE")
-        return None
+        return None, changed_tiles
 
-    total_changed_tiles = 0
+    
     newly_discovered = 0
     rewritten_tiles = 0
     i = 0
     for element_new in msg.data:
         if element_new != old_map[i]:
-            total_changed_tiles = total_changed_tiles + 1
+            changed_tiles += 1
             if old_map[i] == -1:
                 newly_discovered = newly_discovered + 1
-    if total_changed_tiles != newly_discovered:
-        rewritten_tiles = total_changed_tiles - newly_discovered
-    print map_name + "/total tiles changed since last update: " + str(total_changed_tiles)
+        i += 1
+    if changed_tiles != newly_discovered:
+        rewritten_tiles = changed_tiles - newly_discovered
+    print map_name + "/total tiles changed since last update: " + str(changed_tiles)
     print map_name + "/newly_discovered "+str(newly_discovered)
     print map_name + "/tiles rewritten: " + str(rewritten_tiles)
-    monitor.addValue(map_name+"/total_changed_tiles",str(total_changed_tiles), "", 0)
+    monitor.addValue(map_name+"/changed_tiles",str(changed_tiles), "", 0)
     monitor.addValue(map_name+"/newly_discovered_tiles",str(newly_discovered), "", 0)
     monitor.addValue(map_name+"/rewritten_tiles",str(rewritten_tiles), "", 0)
-
-    monitor.publish()
-
+    
     oldMap = msg.data
     print str(len(oldMap)) +""+ map_name
-    return oldMap
+    return (oldMap, changed_tiles)
 
 if __name__ == '__main__':
     init()
